7a56ccf612ce91d9c6aa5c36633d3ad1c455e881
Amoson <pengjiashu@vip.qq.com>
2016-11-12 18:14:47 +0800

    update

--- page/test/index.js
+++ page/test/index.js
@@ -7,20 +7,25 @@ Page({
   startX: 0,
   startY: 0,
   touchStart: function (e) {
-      this.startX = e.changedTouches[0].x
-      this.startY = e.changedTouches[0].y
+      this.startX = e.touches[0].x
+      this.startY = e.touches[0].y
       this.context = wx.createContext()
 
       this.context.setStrokeStyle("#000000")
-      this.context.setLineWidth(10)
+      this.context.setLineWidth(2)
       this.context.setLineCap('round') // 让线条圆润
       this.context.beginPath()
   },
   touchMove: function (e) {
       this.context.moveTo(this.startX, this.startY)
-      this.startX = e.changedTouches[0].x
-      this.startY = e.changedTouches[0].y
+      this.startX = e.touches[0].x
+      this.startY = e.touches[0].y
       this.context.lineTo(this.startX, this.startY)
+      wx.showToast({
+        title: this.startX + ',' + this.startY,
+        icon: 'success',
+        duration: 2000
+      })
       this.context.stroke()
       wx.drawCanvas({
          canvasId: 'firstCanvas',
